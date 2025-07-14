f24e9f72843cb616060b49788107f1b777859903
oguzhndlc <oguzdalg@gmail.com>
2025-07-15 02:44:52 +0300

    Update veriEkle.js

--- netlify/functions/veriEkle.js
+++ netlify/functions/veriEkle.js
@@ -1,27 +1,33 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString: process.env.NETLIFY_DATABASE_URL‎,
-  ssl: { rejectUnauthorized: false }
-});
+const { Client } = require('pg');
 
 exports.handler = async function(event) {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Sadece POST istekleri.' };
   }
 
+  const client = new Client({
+    connectionString: process.env.NETLIFY_DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  });
+
   try {
+    await client.connect();
     const { username } = JSON.parse(event.body);
-    const result = await pool.query(
+
+    const result = await client.query(
       'INSERT INTO deneme (username) VALUES ($1) RETURNING *',
       [username]
     );
+
+    await client.end();
 
     return {
       statusCode: 200,
       body: JSON.stringify({ mesaj: 'Kayıt başarıyla eklendi.', data: result.rows[0] })
     };
+
   } catch (err) {
+    console.error('Hata:', err);
     return {
       statusCode: 500,
       body: JSON.stringify({ hata: err.message })
