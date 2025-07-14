7ff959bb85f243221849ec282937e92466446df4
oguzhndlc <oguzdalg@gmail.com>
2025-07-15 02:23:04 +0300

    Update veriEkle.js

--- netlify/functions/veriEkle.js
+++ netlify/functions/veriEkle.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.NETLIFY_DATABASE_URL‎,
   ssl: { rejectUnauthorized: false }
 });
 
