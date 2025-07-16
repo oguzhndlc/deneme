23b1473cea149fae45f7bdfea677c054abccc082
oguzhndlc <oguzdalg@gmail.com>
2025-07-16 21:46:41 +0300

    Update users_db.js

--- netlify/functions/users_db.js
+++ netlify/functions/users_db.js
@@ -7,7 +7,7 @@ const pool = new Pool({
 
 exports.handler = async function(event, context) {
   try {
-    const result = await pool.query('SELECT * FROM users WHERE id ASC LIMIT 2');
+    const result = await pool.query('SELECT * FROM users ORDER BY id ASC');
     return {
       statusCode: 200,
       body: JSON.stringify(result.rows[0])
