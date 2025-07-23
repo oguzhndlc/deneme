9eb7b6ebf06284c299b26654f0696c5b238c349e
oguzhndlc <oguzdalg@gmail.com>
2025-07-23 03:38:03 +0300

    Update sign_api.js

--- netlify/functions/sign_api.js
+++ netlify/functions/sign_api.js
@@ -8,20 +8,20 @@ exports.handler = async function(event) {
     };
   }
 
-  let user_name, password,name,surname,mail,phone_number;
+  let user_name, password, name, surname, mail, phone_number;
+
   try {
     const bodyDecoded = Buffer.from(event.body, 'base64').toString('utf8');
     const parsedBody = JSON.parse(bodyDecoded || event.body);
 
     user_name = parsedBody.username;
     password = parsedBody.password;
-    name = parsedBody.username;
-    surname = parsedBody.username;
-    mail = parsedBody.username;
-    phone_number = parsedBody.username;
+    name = parsedBody.name;
+    surname = parsedBody.surname;
+    mail = parsedBody.mail;
+    phone_number = parsedBody.phone_number;
 
-    
-    if (!username || !password) {
+    if (!user_name || !password) {
       return {
         statusCode: 400,
         body: JSON.stringify({ hata: 'username ve password zorunludur.' })
@@ -44,8 +44,8 @@ exports.handler = async function(event) {
     await client.connect();
 
     const result = await client.query(
-      'INSERT INTO accounts (user_name, passwrd,name,surname,mail,phone_number) VALUES ($1, $2,$3,$4,$5,$6) RETURNING user_name',
-      [username, password,name, surname,mail,phone_number]
+      'INSERT INTO accounts (user_name, passwrd, name, surname, mail, phone_number) VALUES ($1, $2, $3, $4, $5, $6) RETURNING user_name',
+      [user_name, password, name, surname, mail, phone_number]
     );
 
     await client.end();
