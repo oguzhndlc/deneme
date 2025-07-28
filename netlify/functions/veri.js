ab9dc72967f9e419b758af9c6c7375c96f87c4da
oguzhndlc <oguzdalg@gmail.com>
2025-07-29 02:54:17 +0300

    Update veri.js

--- netlify/functions/veri.js
+++ netlify/functions/veri.js
@@ -8,16 +8,16 @@ const pool = new Pool({
 exports.handler = async function(event, context) {
   try {
     const result = await pool.query('SELECT * FROM accounts ORDER BY id ASC');
-    const result = await pool.query('SELECT * FROM costumes ORDER BY id ASC');
-    const result = await pool.query('SELECT * FROM user_settings ORDER BY id ASC');
-    const result = await pool.query('SELECT * FROM user_stats ORDER BY id ASC');
-    const result = await pool.query('SELECT * FROM user_skins ORDER BY id ASC');
+    const result1 = await pool.query('SELECT * FROM costumes ORDER BY id ASC');
+    const result2 = await pool.query('SELECT * FROM user_settings ORDER BY id ASC');
+    const result3 = await pool.query('SELECT * FROM user_stats ORDER BY id ASC');
+    const result4 = await pool.query('SELECT * FROM user_skins ORDER BY id ASC');
 
 
     return {
       statusCode: 200,
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ accounts: result.rows })   // Tüm kullanıcılar JSON dizisi olarak
+      body: JSON.stringify({ accounts: result.rows,costumes: result1.rows,user_settings: result2.rows,user_stats: result3.rows,user_skins: result4.rows})
     };
 
   } catch (err) {
